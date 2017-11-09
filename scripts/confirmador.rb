a6789c6f7a34d0043a880855202c1048b5df25e5
joaovitor123jv <jv.joaovitoras@gmail.com>
2017-11-09 18:29:57 -0200

    Inicio de desenvolvimento de confirmador, BKP antes de viagem

--- scripts/confirmador.rb
+++ scripts/confirmador.rb
@@ -1,1 +1,54 @@
 #Fazer esse negócio enviar um email de confirmação
+#
+#Envia um email pra algum lugar, usando SMTP
+#
+#
+#require 'net/smtp'
+#
+#message = <<MESSAGE_END
+#From: Eu mesmo Person <jv.joaovitoras@gmail.com>
+#To: A Test User <jv.joaovitoras@outlook.com>
+#Subject: Teste e-mail teste
+#
+#This is a test e-mail message.
+#MESSAGE_END
+#
+#Net::SMTP.start('localhost') do |smtp|
+#  smtp.send_message message, 'jv.joaovitoras@gmail.com', 
+#                             'jv.joaovitoras@outlook.com'
+#end
+#
+#
+#
+#
+#Pro metodo abaixo funcionar, execute 'gem install mail'
+
+
+
+require 'mail'
+
+
+mail = Mail.new do
+	from 'jv.joaovitoras@gmail.com'
+	to	'jv.joaovitoras@outlook.com'
+	subject	'Um email de teste'
+	body File.read('body.txt')
+end
+
+mail.to_s
+
+mail.delivery_method :sendmail
+
+
+mail.deliver!
+
+#if( mail.bounced? )
+#	puts "BOUNCED"
+#	puts mail.final_recipient
+#	puts mail.action
+#	puts mail.error_status
+#	puts mail.diagnostic_code
+#	puts mail.retryable?
+#end
+
+puts "Email enviado ou não"
